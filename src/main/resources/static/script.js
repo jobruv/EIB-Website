52137b0d8589111ae988ff1fe00d1a94b8269c96
Serena <serenauzumcu@protonmail.com>
2026-06-24 15:49:10 +0300

    finished main page
    everything except announcement and database connection

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -44,3 +44,24 @@ document.getElementById("heroPrev").addEventListener("click", showPreviousHeroSl
 document.getElementById("heroNext").addEventListener("click", showNextHeroSlide);
 
 renderHeroSlide(heroIndex);
+document.addEventListener("click", function (e) {
+    const isDropdownBtn = e.target.closest(".dropbtn");
+
+    if (!isDropdownBtn && e.target.closest(".dropdown-content") === null) {
+        document.querySelectorAll(".dropdown.show").forEach(drop => {
+            drop.classList.remove("show");
+        });
+        return;
+    }
+
+    if (isDropdownBtn) {
+        e.preventDefault();
+        const currentDropdown = isDropdownBtn.closest(".dropdown");
+    
+        document.querySelectorAll(".dropdown.show").forEach(drop => {
+            if (drop !== currentDropdown) drop.classList.remove("show");
+        });
+
+        currentDropdown.classList.toggle("show");
+    }
+});
